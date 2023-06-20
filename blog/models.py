b5162d96476109c456c99dce3b0da1410b670cd8
Matheus Pinheiro <78563956+MatheeusPinheiro@users.noreply.github.com>
2023-06-19 20:40:11 -0400

    adicionando o ckeditor

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from datetime import datetime
-
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField(blank=False, null=False)
     data_publicacao = models.DateTimeField(default=datetime.now())
     
     
     def __str__(self):
         return "{} -  {}".format(self.autor, self.titulo)
+
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
+    "ckeditor",
+    "ckeditor_uploader",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
+MEDIA_URL = 'images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
